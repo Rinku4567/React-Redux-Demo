1df20afd6d1b1f69a98b0bac15e8bd6007366f1f
Rinku Bathra <rinku45210@gmail.com>
2025-10-01 11:41:45 +0530

    Three functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
+import Cakecontainer from './components/Cakecontainer'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-   <h1>Hello world</h1>
+   <>
+   <Provider store={store}>
+   <Cakecontainer/>
+   </Provider>
     </>
   )
 }
---
+++ src/components/Cakecontainer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {buyCake} from "../redux"
+
+function Cakecontainer(props) {
+  return (
+    <div>
+      <h1>Number of cakes-{props.numOfCakes}</h1>
+      <button onClick={props.buyCake1}>Buy cake</button>
+    </div>
+  )
+}
+
+// It change the numOfCakes state in props so when we click on the button so it change the state of the store object props.numOfCakes
+const mapStateToProps=(state)=>{
+  return{
+    numOfCakes:state.numOfCakes
+  }
+}
+
+// It create a buyCake prop which is a function so it create props.buyCake prop
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    buyCake1:()=>dispatch(buyCake())
+  }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps) (Cakecontainer)
